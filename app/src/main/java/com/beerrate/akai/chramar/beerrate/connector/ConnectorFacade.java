e36dc00f9c034f97740918fdca1f492b7d99040b
janek@projmen.pl <janek@projmen.pl>
2017-11-27 12:49:16 +0100

    Add facade to connect with database

---
+++ app/src/main/java/com/beerrate/akai/chramar/beerrate/connector/ConnectorFacade.java
@@ -0,0 +1,26 @@
+package com.beerrate.akai.chramar.beerrate.connector;
+
+
+import com.beerrate.akai.chramar.beerrate.datamodel.Beer;
+
+import java.util.List;
+
+public class ConnectorFacade {
+
+    private BeerDataProvider beerDataProvider;
+    private BeerDataSender beerDataSender;
+
+    public void addBeer(Beer beer){
+        beerDataSender.addBeer(beer);
+    }
+
+    public List<Beer> getAll(){
+        return beerDataProvider.getAll();
+    }
+
+    public Beer getBeerById(long id){
+        return beerDataProvider.getBeerById(id);
+    }
+
+
+}
