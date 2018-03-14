80e7e3e912a58aa7b53048cac518e76ae5bc3826
chramar <marchr7@gmail.com>
2018-03-14 13:11:16 +0100

    init fot dataactivity

--- app/src/main/java/com/beerrate/akai/chramar/beerrate/datamodel/Beer.java
+++ app/src/main/java/com/beerrate/akai/chramar/beerrate/datamodel/Beer.java
@@ -1,6 +1,13 @@
 package com.beerrate.akai.chramar.beerrate.datamodel;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
 public class Beer {
+
     private long id;
     private String name;
     private String brewery;
@@ -21,67 +28,6 @@ public class Beer {
         this.price = price;
     }
 
-    public long getId() {
-        return id;
-    }
 
-    public void setId(long id) {
-        this.id = id;
-    }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getBrewery() {
-        return brewery;
-    }
-
-    public void setBrewery(String brewery) {
-        this.brewery = brewery;
-    }
-
-    public String getStyle() {
-        return style;
-    }
-
-    public void setStyle(String style) {
-        this.style = style;
-    }
-
-    public String getWhereBought() {
-        return whereBought;
-    }
-
-    public void setWhereBought(String whereBought) {
-        this.whereBought = whereBought;
-    }
-
-    public int getIbu() {
-        return ibu;
-    }
-
-    public void setIbu(int ibu) {
-        this.ibu = ibu;
-    }
-
-    public float getAlc() {
-        return alc;
-    }
-
-    public void setAlc(float alc) {
-        this.alc = alc;
-    }
-
-    public float getPrice() {
-        return price;
-    }
-
-    public void setPrice(float price) {
-        this.price = price;
-    }
 }
